24804bc6a3a1848517736bbcdc97fe65be79997a
Andreas Røssland <andreas.roessland@gmail.com>
2021-11-21 02:00:43 +0100

    fix it again

--- internal/api/handlers/api-health.go
+++ internal/api/handlers/api-health.go
@@ -8,10 +8,6 @@ import (
 
 func GetApiHealth(app *server.App) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// an example API handler
-		w.WriteHeader(400)
-		return
-
 		w.Header().Set("Cache-Control", "no-cache, private, max-age=0")
 		err := json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 		if err != nil {
